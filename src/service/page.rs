4736dd52c0827596120352c691a9ccf45978759a
dayang <1306693844@qq.com>
2020-04-30 18:40:11 +0800

    删除无用文件

--- src/service/page.rs
+++ src/service/page.rs
@@ -5,14 +5,14 @@ use crate::schema::page::{dsl as page_dsl, dsl::*};
 use diesel::prelude::*;
 use std::collections::HashMap;
 
-pub fn get_book_catalog(conn: &SqliteConnection, book_id: i32) -> Result<Vec<CatalogItem>, String> {
+pub fn get_book_catalog(conn: &SqliteConnection, the_book_id: i32) -> Result<Vec<CatalogItem>, String> {
     let page_items = page
-        .filter(page_dsl::book_id.eq(book_id))
+        .filter(page_dsl::book_id.eq(the_book_id))
         .select((id, parent_id, display_order, url))
         .load::<(i32, i32, i32, String)>(conn)
         .map_err(err_str)?;
 
-    let mut map = HashMap::new();
+    //let mut map = HashMap::new();
     let mut catalog_items = vec![];
     // for item in page_items {
         
